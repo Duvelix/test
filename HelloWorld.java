deb81e93be3c7610875c5305db1b2f81678a6d49
Duvelix <duvelix@keepmind.net>
2020-01-23 16:27:50 +0900

    Master branch commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		System.out.println("This is only visible on the 'another' branch.");
+		System.out.println("This is only visible on the 'master' branch.");
 	}
 
 }
